cdd32b5d3afb504d49daf6ea10777cafa23a87fe
Seth DImmer <sethdimmer1@gmail.com>
2023-02-01 13:59:04 -0500

    initial commit

---
+++ get element by ID VID 91./app.js
@@ -0,0 +1,24 @@
+// Select the element or group of elements that i want
+
+// Decide the affect i want to apply to the selection.
+
+
+// getElementById('element')
+
+const h1 = document.getElementById('title');
+h1.style.color = 'red' // changed the color of the h1 tag
+
+const btn = document.getElementById('btn');
+
+btn.style.backgroundColor = 'blue';
+btn.style.color = 'white';
+
+
+// i stored the getElementById in a variable so i can have access to it
+// as well as putting properties on it to change it 
+
+// if there is multiple bunttons and i want them all them same styling this is great way to do it.
+
+
+
+
