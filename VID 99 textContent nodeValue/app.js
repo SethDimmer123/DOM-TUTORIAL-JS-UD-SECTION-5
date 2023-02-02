d181249410ce4791958276d0ff2f171b55eb9dbc
Seth DImmer <sethdimmer1@gmail.com>
2023-02-02 09:15:12 -0500

    initial commit

--- VID 99 textContent nodeValue/app.js
+++ VID 99 textContent nodeValue/app.js
@@ -8,6 +8,9 @@ const easyValue = item.textContent;
 console.log(easyValue);
 
 // these 2 properties allow me to access the textContent 
-// WITHIN THE ELEMENT
+// WITHIN THE ELEMENT to print in the console
+
+// in the console i should see "this is special content"
+// based on my accessing the special h1 id.
 
 // THEY ALSO DO THE SAMETHING
