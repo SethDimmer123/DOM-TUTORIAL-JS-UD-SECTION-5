9216cea355d4880e188f270765ba15954506e85f
Seth DImmer <sethdimmer1@gmail.com>
2023-02-02 18:09:17 -0500

    initial commit

---
+++ VID 106. remove removeChild/app.js
@@ -0,0 +1,24 @@
+// remove
+// removeChild
+
+// these 2 methods allow me to remove elements from the DOM
+
+const result = document.querySelector('#result');
+// result.remove()
+// dynamically my div id is not on the page anymore.
+
+
+
+// i want to remove the second heading
+// in order to use removeCHild i need to run it on a parent
+
+// so result in this case is the parent
+
+const heading = result.querySelector('h1');
+result.removeChild(heading);
+console.log(heading);
+
+
+
+
+
