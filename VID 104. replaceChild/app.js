0f061b1de12d820d60bb6caf1c2a61af22374beb
Seth DImmer <sethdimmer1@gmail.com>
2023-02-02 17:14:14 -0500

    initial commit

---
+++ VID 104. replaceChild/app.js
@@ -0,0 +1,36 @@
+// createElement('element')
+// createTextNode('text content')
+// element.appendChild(childElement)
+
+// replaceChild('new','old');
+
+// TAKES 2 ARGUEMENTS
+
+// 1. new element- what element i am going to be placing.
+// 2. old element- which element i am going to be REPLACING
+
+const result = document.querySelector('#result');
+const first = document.querySelector('.red');
+// create empty element
+
+const bodyDiv = document.createElement('div');
+
+
+// create text node
+const text = document.createTextNode('a simple body div');
+bodyDiv.appendChild(text);
+document.body.insertBefore(bodyDiv, result);
+// result element
+const heading = document.createElement('h2');
+const headingText = document.createTextNode('dynamic heading');
+heading.appendChild(headingText);
+heading.classList.add('blue');
+
+result.insertBefore(heading, first);
+
+const smallHeading = document.createElement('h6');
+const smallText = document.createTextNode(`i'm small heading text`);
+smallHeading.classList.add('red');
+smallHeading.appendChild(smallText);
+document.body.replaceChild(smallHeading, bodyDiv);
+console.log(result.children);
