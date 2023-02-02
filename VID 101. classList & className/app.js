2822046a428e9f021b00d2c2972cff7c28f6be51
Seth DImmer <sethdimmer1@gmail.com>
2023-02-02 13:58:48 -0500

    initial commit

--- VID 101. classList & className/app.js
+++ VID 101. classList & className/app.js
@@ -5,23 +5,32 @@ const first = document.getElementById('first');
 const second = document.getElementById('second');
 const third = document.getElementById('third');
 
-const classValue = first.className; // className allows me to CHECK for the className
+// const classValue = first.className; 
+// className allows me to CHECK for the className
 // as well as CHANGE the className OR ADD A NEW className. FROM index.html
-console.log(classValue);
-
-// second.className = 'colors text';
-
-// third.classList.add('colors');
-// third.classList.add('text');
-// third.classList.add('text', 'colors');
-// third.classList.remove('text');
-
-// let result = third.classList.contains('colors');
-// if (result) {
-//   console.log('hello world');
-// } else {
-//   console.log('does not have the class');
-// }
-
-// const classValue = third.classList;
 // console.log(classValue);
+
+second.className = 'text colors'; // second element great example on adding 
+// a new class name "colors"
+
+
+
+
+third.classList.add('colors'); // i add the css class of colors to the third element
+third.classList.add('text');
+third.classList.add('text', 'colors');
+third.classList.remove('text');
+
+let result = third.classList.contains('colors'); //contains checks 'colors' if the particaular html element has the class i want to edit
+if (result) {
+  console.log('hello world');
+} else {
+  console.log('does not have the class');
+}
+
+// if the 3rd html element has the class then console log 'hello world'.
+
+// if the 3rd html element DOES NOT HAVE the class then console log the else statement
+
+const classValue = third.classList;
+console.log(classValue);
