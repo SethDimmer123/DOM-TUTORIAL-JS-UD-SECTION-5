ca55cdeb247bfc6566ef3d9272e4a9c0e3225c73
Seth DImmer <sethdimmer1@gmail.com>
2023-02-02 17:55:38 -0500

    initial commit

--- VID 105. prepend innerText/app.js
+++ VID 105. prepend innerText/app.js
@@ -1,6 +1,10 @@
-// prepend
-// innerText
+// prepend method
+// innerText-property
+
+// this allows me to set text right away instead of 
+// creating a textNode and appending to whatever element
 
 const heading = document.createElement('h2');
 heading.innerText = `i am a dynamic heading`;
-document.body.prepend(heading);
+document.body.prepend(heading); //prepend instead of appened puts my heading in front of the index.html heading 1 element
+
