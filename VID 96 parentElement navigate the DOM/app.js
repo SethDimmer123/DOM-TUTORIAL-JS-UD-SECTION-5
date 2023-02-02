8ed98d28a8c3350ba03d930b6356ede7c431d90d
Seth DImmer <sethdimmer1@gmail.com>
2023-02-02 07:55:48 -0500

    initial commit

--- VID 96 parentElement navigate the DOM/app.js
+++ VID 96 parentElement navigate the DOM/app.js
@@ -1,5 +1,8 @@
 // parentElement
 
 const heading = document.querySelector('h2');
-console.log(heading.parentElement.parentElement); 
+// console.log(heading.parentElement.parentElement); 
 // the div class of second is the parent element of the second heading
+
+const parent = heading.parentElement;
+parent.style.color = 'red';
