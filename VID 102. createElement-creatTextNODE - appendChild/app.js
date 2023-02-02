23263c6b6100ac6b8e0dff17bb0e63447f5d1f76
Seth DImmer <sethdimmer1@gmail.com>
2023-02-02 15:55:00 -0500

    initial commit

--- VID 102. createElement-creatTextNODE - appendChild/app.js
+++ VID 102. createElement-creatTextNODE - appendChild/app.js
@@ -4,21 +4,32 @@
 // createTextNode('text content')
 // element.appendChild(childElement)
 
-const result = document.querySelector('#result');
+
+//  ONLY WHEN JAVASCRIPT RUNS THEN WE CREATE OUR DIV ELEMENT 
+// THEN THE textNode
+// then add the textnode to  body div
+//then the body div gets append to the body element
+
+const result = document.querySelector('#result'); // parent of appendChild
 
 // create empty element
 
 const bodyDiv = document.createElement('div');
-// create text node
-const text = document.createTextNode('a simple body div');
-bodyDiv.appendChild(text);
+// // create text node
+const text = document.createTextNode('a simple body div');// the text on the browser page
+bodyDiv.appendChild(text); //what kind of element do i want to add to my body div?
 document.body.appendChild(bodyDiv);
 
-const heading = document.createElement('h2');
-const headingText = document.createTextNode('dynamic heading');
-heading.appendChild(headingText);
+
+
+
+const heading = document.createElement('h2'); // we created a h2 element
+const headingText = document.createTextNode('dynamic heading');  // i created a textNode 
+// called dynamic heading which the same thing as a text string.
+heading.appendChild(headingText); // child of div id="result"
 heading.classList.add('blue');
 
-result.appendChild(heading);
+result.appendChild(heading); // child of div id="result"
 
-console.log(result.children);
+console.log(result.children); // the .children property returns the children 
+// elements in the console.
