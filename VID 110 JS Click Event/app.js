baa55fdd2d6863e8a1582428ee0f26b043998245
Seth DImmer <sethdimmer1@gmail.com>
2023-02-04 15:38:32 -0500

    initial commit

--- VID 110 JS Click Event/app.js
+++ VID 110 JS Click Event/app.js
@@ -1,10 +1,15 @@
+// core principles about events
+
 // select element
 // addEventListener()
-// what event, what to do
+// what event am i going to be listening for AND  what to do WITH it(callback function)
 
-const btn = document.querySelector('.btn');
+const btn = document.querySelector('.btn'); // i am LISTENING FOR THE CLICK EVENTS ON A BUTTON.
 const heading = document.querySelector('h2');
 
 btn.addEventListener('click', function() {
   heading.classList.add('red');
 });
+
+//lets say a user clicks a button what is it that i would want to do.
+
