ca016ae8b4772431625d0da239bda70f583f443b
Seth DImmer <sethdimmer1@gmail.com>
2023-02-04 14:16:26 -0500

    initial commit

--- VID 108. Change CSS with style property/app.js
+++ VID 108. Change CSS with style property/app.js
@@ -1,10 +1,16 @@
 // CSS
 
-const random = document.querySelector('.random');
+const random = document.querySelector('.random'); // editing the class of random
+
+// the DOWNSIDE OF USING THE style Property is i am adding CSS PROPERTIES 1 at a time
 
 // random.style.backgroundColor = 'blue';
 // random.style.color = 'white';
 // random.style.fontSize = '3rem';
 // random.style.textTransform = 'capitalize';
 
+// instead of doing what is above just use the classList so i can use ALL OF THE CSS PROPERTIES
+// INSTEAD OF ADDING THEM 1 AT A TIME
+
 random.classList.add('title');
+
