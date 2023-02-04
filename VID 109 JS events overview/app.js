522cc0bd151c9c1bfce062eabd673b36309b0941
Seth DImmer <sethdimmer1@gmail.com>
2023-02-04 14:36:14 -0500

    initial commit

--- VID 109 JS events overview/app.js
+++ VID 109 JS events overview/app.js
@@ -5,6 +5,8 @@ const text = document.querySelector('p');
 text.addEventListener('mouseenter', function() {
   heading.classList.add('red');
 });
+
+// THE MOMENT I ENTER THE AREA OF THE p tag the mouseenter event listener activates.
 text.addEventListener('mouseleave', function() {
   heading.classList.remove('red');
 });
@@ -15,3 +17,19 @@ btn.addEventListener('click', function() {
     heading.classList.add('blue');
   }
 });
+
+//when i click the button the h2 background color is activated.
+
+
+// an event could be a simple mouse click or windows scroll
+
+// or a form submission
+
+// these are example of events in javascript
+
+// we will only cover a couple NOT ALL OF THEM 
+
+// I JUST NEED TO UNDERSTAND THE PRINCIPLES BEHIND THE EVENTS.
+
+// ONCE I UNDERSTAND THE PRINCIPLES
+// THEN IT WILL BE EASIER TO LEARN A NEW EVENT
