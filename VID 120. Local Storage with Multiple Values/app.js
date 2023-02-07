a6275eaf86d09f8514e29b503078cc5343e106fd
Seth DImmer <sethdimmer1@gmail.com>
2023-02-07 10:12:55 -0500

    initial commit

--- VID 120. Local Storage with Multiple Values/app.js
+++ VID 120. Local Storage with Multiple Values/app.js
@@ -1,19 +1,31 @@
-// JSON.stringify(), JSON.parse
+// JSON.stringify() -- converts my value to JSON STRING
+// , JSON.parse -- gets back my initial value.
 
 const friends = ['john', 'peter', 'bob'];
-localStorage.setItem('friends', JSON.stringify(friends));
+localStorage.setItem('friends', JSON.stringify(friends)); 
+
+//when i work with localStorage and the value is more complicated 
+// than just a simple string in this case it was an array
+//  i need to store it using 
+// JSON.STRINGIFY first --pass my value which in this case is friends
+// then when i am accessing the value i need to have JSON.parse then pass in localStorage.getItem
+// ino rder to keep the data structure i had in this case it is an array.
 
 const values = JSON.parse(localStorage.getItem('friends'));
-console.log(values[2]);
+console.log(values[2]); 
 
-let fruits;
 
-if (localStorage.getItem('fruits')) {
-  fruits = JSON.parse(localStorage.getItem('fruits'));
-} else {
-  fruits = [];
-}
-console.log(fruits);
-// fruits.push('apple');
-fruits.push('orange');
-localStorage.setItem('fruits', JSON.stringify(fruits));
+
+
+
+// let fruits;
+
+// if (localStorage.getItem('fruits')) {
+//   fruits = JSON.parse(localStorage.getItem('fruits'));
+// } else {
+//   fruits = [];
+// }
+// console.log(fruits);
+// // fruits.push('apple');
+// fruits.push('orange');
+// localStorage.setItem('fruits', JSON.stringify(fruits));
