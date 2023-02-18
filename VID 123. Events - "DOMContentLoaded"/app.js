7629472d95fd6c9d0eadb4ceb5731548be9c4b94
Seth DImmer <sethdimmer1@gmail.com>
2023-02-18 17:47:56 -0500

    initial commit

--- VID 123. Events - "DOMContentLoaded"/app.js
+++ VID 123. Events - "DOMContentLoaded"/app.js
@@ -1,5 +1,7 @@
 /* 
-The DOMContentLoaded event fires when the initial HTML document has been completely loaded and parsed, without waiting for stylesheets, images, and subframes to finish loading.
+The DOMContentLoaded event fires when the initial HTML 
+document has been completely loaded and parsed, without waiting for 
+stylesheets, images, and subframes to finish loading.
 
 window.addEventListener('DOMContentLoaded', function () {
   // your code goes here
